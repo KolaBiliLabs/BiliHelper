66660fad6f26901e7747cf3670105c718886dfe0
imddc <easyhoo2426@163.com>
2025-06-18 17:23:25 +0800

    feat: tailwind and eslint

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+</script>
+
+<template>
+  <div class="bg-amber-500">
+    12312321321
+  </div>
+</template>
