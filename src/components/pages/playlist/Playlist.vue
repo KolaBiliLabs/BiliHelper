5d90d39f46060ea59a4ca19bda220677353d8509
imddc <easyhoo2426@163.com>
2025-07-14 14:56:54 +0800

    style: 美化歌单页面 header 区域样式

--- src/components/pages/playlist/Playlist.vue
+++ src/components/pages/playlist/Playlist.vue
@@ -1,13 +1,32 @@
 <script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import { computed } from 'vue'
+import { usePlaylistStore } from '@/stores/playlistStore'
+import { useSystemStore } from '@/stores/systemStore'
+import { formatTime } from '@/utils/dayjs'
+import SongList from '../searchResult/SongList.vue'
 
+const systemStore = useSystemStore()
+const { currentPage } = storeToRefs(systemStore)
+
+const playlistStore = usePlaylistStore()
+const { playlists } = storeToRefs(playlistStore)
+
+const playList = computed(() => playlists.value.find(v => v.id === currentPage.value)!)
+
+function chooseSong(v) {
+  console.log('choose song in Playlist', v)
+}
 </script>
 
 <template>
   <div>
-    play list page
+    <header class="space-y-1">
+      <h1>{{ playList.name }}</h1>
+      <template v-if="!playList.isDefault">
+        {{ formatTime(playList.createTime, 'YYYY-MM-DD') }} 创建
+      </template>
+    </header>
+    <SongList :data="playList.musics" @choose="chooseSong" />
   </div>
 </template>
-
-<style scoped>
-
-</style>
