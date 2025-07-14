90ac850b947f8eff5a9b8a21f672627cfd76bb46
imddc <easyhoo2426@163.com>
2025-07-14 13:04:57 +0800

    feat: 新增页面常量与歌单页面目录

---
+++ constants/pageId.ts
@@ -0,0 +1,1 @@
+export const SEARCH_RESULT_PAGE = 'searchResult'
---
+++ src/components/pages/playlist/Playlist.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    play list page
+  </div>
+</template>
+
+<style scoped>
+
+</style>
