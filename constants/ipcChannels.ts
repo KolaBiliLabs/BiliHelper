68359db035aecf8b163bb7f8d0f57d4214b8a0c3
imddc <easyhoo2426@163.com>
2025-07-30 22:25:23 +0800

    feat: 将 ipc channels 统一提取出

---
+++ constants/ipcChannels.ts
@@ -0,0 +1,14 @@
+// 播放channels
+export const LIKE_STATUS_CHANGE = 'like_status_change'
+export const PLAY_STATUS_CHANGE = 'play_status_change'
+
+export const PLAY = 'play'
+export const PAUSE = 'pause'
+export const PLAY_PREV = 'play_prev'
+export const PLAY_NEXT = 'play_next'
+
+// 窗口 channels
+export const /** 最小化窗口 */ WINDOW_MINIMIZE = 'win_min'
+export const /** 最大化窗口 */ WINDOW_MAXIMIZE = 'win_max'
+export const /** 关闭窗口 */ WINDOW_CLOSE = 'win_close'
+export const /** 是否最大化 */ WINDOW_IS_MAXIMIZED = 'win_is_max'
